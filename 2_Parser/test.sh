42111e3cac25f2ecab5a735f6e5b0ce4085d396f
SeongIkKim <kpic1638@gmail.com>
2023-11-10 20:35:12 +0900

    test: add test for dangling else, null return

--- 2_Parser/test.sh
+++ 2_Parser/test.sh
@@ -2,7 +2,7 @@
 
 echo "##### parser test #####"
 
-for i in {1..2}; do
+for i in {1..4}; do
     output=$(./cminus_parser "example/test.${i}.txt" | diff - "example/result.${i}.txt")
     if [ -z "$output" ]; then
         echo "==============>test $i 통과"
